b58dd6ab8d6a83b5231e8b44932f7f766c90f6df
Jhon Moofarrif <jhon.moofarrifgmail.com>
2024-06-17 00:36:50 -0500

    authApp angular

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+})
+export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['jhon@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    const { email, password } = this.myForm.value;
+    this.authService.login(email, password).subscribe({
+      next: () => this.router.navigateByUrl('/dashboard'),
+      error: (message: string) => Swal.fire('Error', message, 'error'),
+    });
+  }
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  BASE_URL: 'https://nestjs-auth-backend-moofarrif.up.railway.app',
+};
